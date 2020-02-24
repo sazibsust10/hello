4e7e72328df7cb4e07cfcef4a7cb66b08509dcd4
M M Mahmudul Hassan <sazibsust10@gmail.com>
2020-02-24 21:08:33 +0100

    first commit

---
+++ src/component/inputBox.js
@@ -0,0 +1,29 @@
+
+import React, { Component, createRef } from "react";
+
+class inputBox extends Component {
+    constructor() {
+      super();
+      this.state = {
+        
+      };
+    }
+  
+    componentDidMount() {
+      
+      
+    }
+  
+    onClickMaker(){
+      console.log("Here");
+    }
+  
+    render() {
+  
+      return (
+        <h1> asdfasdf </h1>
+      );
+    }
+  }
+export default inputBox;
+
